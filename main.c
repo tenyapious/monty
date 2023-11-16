ad7945af8b124fe27662cdf7a7bea3d90207a7c3
tenyapious <tenyapious@gmail.com>
2023-11-16 12:08:56 +0000

    cut main function to less than 40 lines

--- main.c
+++ main.c
@@ -4,6 +4,29 @@
 #include "monty.h"
 
 /**
+ * exec_line - execute line from monty bytecode
+ * @stack: pointer to first node of the stack
+ * @opcode: instruction to be executed
+ * @line_number: line number of opcode
+*/
+void exec_line(stack_t **stack, char *opcode, int line_number)
+{
+		if (strcmp(opcode, "push") == 0)
+		{
+			add_node(stack, line_number);
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			print_all(stack);
+		}
+		else
+		{
+			fprintf(stderr, "L%d: unknown instruction %s", line_number, opcode);
+			exit(EXIT_FAILURE);
+		}
+}
+
+/**
  * main - interprete monty bytecode
  * @ac: arguments coount
  * @av: arguments vector
@@ -12,7 +35,7 @@
 */
 int main(int ac, char **av)
 {
-	stack_t *stack __attribute__((unused));
+	stack_t *stack;
 	char *opcode;
 	unsigned int line_number;
 	FILE *file;
@@ -42,20 +65,7 @@ int main(int ac, char **av)
 		{
 			line_number = atoi(token);
 		}
-
-		if (strcmp(opcode, "push") == 0)
-		{
-			add_node(&stack, line_number);
-		}
-		else if (strcmp(opcode, "pall") == 0)
-		{
-			print_all(&stack);
-		}
-		else
-		{
-			fprintf(stderr, "L%d: unknown instruction %s", line_number, opcode);
-			exit(EXIT_FAILURE);
-		}
+		exec_line(&stack, opcode, line_number);
 	}
 
 	free_all(&stack);
