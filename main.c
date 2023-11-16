e01db0b0ab8260dfb01228c27f5fca0e8fb4be32
tenyapious <tenyapious@gmail.com>
2023-11-16 19:33:50 +0000

    extract line reading into a separate function

--- main.c
+++ main.c
@@ -61,33 +61,18 @@ void exec_line(stack_t **stack, char *opcode, int line_number)
 }
 
 /**
- * main - interprete monty bytecode
- * @ac: arguments coount
- * @av: arguments vector
- *
- * Return: 0 if successfull or exit(EXIT_FAILURE)
- */
-int main(int ac, char **av)
+ * readLine - read line from file
+ * @file: file to read line from
+*/
+void readLine(FILE *file)
 {
-	stack_t *stack;
+	stack_t *stack = NULL;
 	char *opcode;
 	unsigned int line_number;
-	FILE *file;
 	char line[100];
 	const char *delim = " \t\n";
 	char *token = NULL;
 
-	if (ac < 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(av[1], "r");
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-		exit(EXIT_FAILURE);
-	}
 	while (fgets(line, sizeof(line), file) != NULL)
 	{
 		token = strtok(line, delim);
@@ -107,6 +92,33 @@ int main(int ac, char **av)
 		line_number++;
 	}
 	free_all(&stack);
+}
+
+/**
+ * main - interprete monty bytecode
+ * @ac: arguments coount
+ * @av: arguments vector
+ *
+ * Return: 0 if successfull or exit(EXIT_FAILURE)
+ */
+int main(int ac, char **av)
+{
+	FILE *file;
+
+	if (ac < 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(av[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	readLine(file);
+
 	fclose(file);
 	return (0);
 }
